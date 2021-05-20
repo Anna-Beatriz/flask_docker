4eac3ed05a9660038b4b05a8e21353c2a6c658d2
Anna Beatriz <annabeatriz.ms1@gmail.com>
2021-05-20 23:32:10 +0000

    Modificando View

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class Comentario(db.Model):
     __tablename__ = "comentarios"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String(40), nullable=False)
     comentario = db.Column(db.String(200), nullable=False)
     data_com = db.Column(db.DateTime, nullable=False)
 
